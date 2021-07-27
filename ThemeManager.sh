8490d1ab3fe1612eae1e54cea2b83a70ec3ec314
Blattoid <danielbridgewater87@gmail.com>
2021-07-27 20:28:27 +0100

    Generalises ShowError()

--- ThemeManager.sh
+++ ThemeManager.sh
@@ -3,41 +3,37 @@
 TITLE="Powercord Theme Manager"
 SIZE="25 80 10"
 
-# I am assuming this script is being used in conjunction with the FullReinstall.sh script
-# Change these variables if this is not the case.
+# I am assuming this utility is being used in conjunction with the FullReinstall.sh script
+# Change this variable if this is not the case.
 POWERCORD_DIR="$(pwd)/powercord" 
-THEMESOURCE="$(pwd)/Themes" # I've placed my Themes folder alongside the script
+# I've placed my Themes folder alongside the utility, but this can be wherever you keep your themes.
+THEMESOURCE="/tmp" #"$(pwd)/Themes"
 
 ShowError() {
 	whiptail --msgbox\
 		--backtitle "$TITLE" --title "$TITLE"\
-		"INTERNAL ERROR!\n\
-${@:1}\n\
-\n\
-Please verify this script is configured correctly and retry."\
+		"Internal error, unable to proceed.\n${@:1}"\
 		$SIZE
 }
 
 # Sanity checks - Make sure Powercord and its appropriate structure exists
 POWERCORD_THEMES="$POWERCORD_DIR/src/Powercord/themes" # Do NOT modify unless the structure of Powercord has changed
 if [ ! -d "$POWERCORD_DIR" ]; then
-	ShowError "\"$POWERCORD_DIR\" does not appear to exist."
+	ShowError "\"$POWERCORD_DIR\" does not appear to exist.\n\n
+Please verify the location for Powercord is configured correctly and retry."
 	exit 1
 elif [ ! -d "$POWERCORD_THEMES" ]; then
-	ShowError "\"$POWERCORD_THEMES\" does not appear to exist."
+	ShowError "\"$POWERCORD_THEMES\" does not appear to exist.\n\n
+Please verify this utility is configured correctly and retry."
 	exit 1
-	
+
 # What about the user's theme folder? Does that have anything in it?
 elif [ -z "$(ls -A $THEMESOURCE)" ]; then
-	whiptail --msgbox\
-		--backtitle "$TITLE" --title "$TITLE"\
-		"You do not appear to have any themes in\n\
-\"$THEMESOURCE\".\n\
-\n\
-Please add some to that folder and then rerun this utility."\
-		$SIZE
+	ShowError "You do not appear to have any themes in\n\
+\"$THEMESOURCE\".\n\n\
+Please add some to that folder and then rerun this utility."
 	exit 1
-		
+
 else
 	# Create an array of the folders present for Whiptail to use
 	i=0
@@ -45,24 +41,28 @@ else
 	do
 		files[i]=$(( i/3 )) # Entry number
 		files[i+1]="$(basename $folder)" # Theme name
-		files[i+2]="OFF" # Default state should be unchecked
+		files[i+2]="OFF" # Default state should be unchecked - TODO: Make this dependent on if POWERCORD_THEMES has the same theme installed, to make the box reflect the current state.
 		((i+=3)) # Increment index counter
 	done
-	
+
 	# Show the selection menu
 	whiptail --checklist\
 		--scrolltext\
 		--backtitle "$TITLE" --title "$TITLE"\
 		--ok-button "Install" --cancel-button "No, thanks"\
-		"We see you have some Powercord Themes, select the ones you wish to use."\
+		"Please make a selection for themes to install:"\
 		$SIZE\
 		"${files[@]}" # Include the array of items created just now
-		
+
+	# TODO: Detect when the Cancel button is pressed
+
+	# TODO: Parse the IDs returned by Whiptail back into folder paths
+	#	Formula could look something like: $THEMESOURCE/$(( ${files[$ID/3+1]} ))
+	#	NOTE: Might be more worthwhile to just store the paths when building the array?
+
 	# Remove all folders in the Powercord Themes folder
-	#rm -rfv "$POWERCORD_DIR/src/Powercord/themes"
-	
+	#rm -rfv "$POWERCORD_THEMES"
+
 	# Copy chosen folders into the folder
-	#cp -rv $idk "$POWERCORD_DIR/src/Powercord/themes"
-	
-	exit 0
+	#cp -rv $idk "$POWERCORD_THEMES"
 fi
