0e9a82b7e3aefe2a98591bca77ee51e1f640b04f
Blattoid <danielbridgewater87@gmail.com>
2022-01-12 21:56:42 +0000

    Merge update of FrostedGlass into my fork

--- Themes/FrostedGlass-custom/scripts/build.js
+++ Themes/FrostedGlass-custom/scripts/build.js
@@ -1,32 +1,12 @@
-// Used to build scss files to the dist folder.
+// GitHub action used to build scss files to the dist folder.
+// You are not meant to run this locally.
 
-const sass = require('sass');
-const fs = require('fs');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
+const compile = require('./compile');
+const {build} = require('./config.json');
 
-console.log('Building FrostedGlass.css file...');
-
-sass.render({
-	file: 'src/_base.scss',
-	outputStyle: 'expanded'
-}, (err, result) => {
-	if (err) {
-		console.error(err);
-		return false;
-	}
-
-	const newRes = Buffer.from(result.css).toString();
-
-	postcss([autoprefixer])
-		.process(newRes, {
-			from: undefined,
-			to: undefined
-		})
-		.then(postcssRes => {
-			fs.writeFile('dist/FrostedGlass.css', postcssRes.css, (err) => {
-				if (err) console.error(err);
-				else console.log(`Successfully built FrostedGlass.css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
-			})
-		})
+build.forEach(step => {
+	compile({
+		target: step.target,
+		output: [...step.output]
+	});
 })
--- Themes/FrostedGlass-custom/scripts/dev.js
+++ Themes/FrostedGlass-custom/scripts/dev.js
@@ -1,39 +1,22 @@
 // Used to build scss files to the BetterDiscord themes folder.
 
 const chokidar = require('chokidar');
-const sass = require('sass');
-const fs = require('fs');
 const path = require('path');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
+const compile = require('./compile');
+const {name, dev} = require('./config.json');
 
 const dataFolder = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME + "/.local/share");
 const themesFolder = path.join(dataFolder, 'BetterDiscord', 'themes');
 
 chokidar.watch('src', {persistent: true})
-	.on('ready', () => console.log('Watching for changes...'))
+	.on('ready', () => console.log(`Watching for changes...`))
 	.on('change', () => {
-		sass.render({
-			file: 'src/FrostedGlass.theme.scss',
-			outputStyle: 'expanded'
-		}, (err, result) => {
-			if (err) {
-				console.error(err);
-				return false;
-			}
+		console.clear();
 
-			const newRes = Buffer.from(result.css).toString();
-		
-			postcss([autoprefixer])
-				.process(newRes, {
-					from: undefined,
-					to: undefined
-				})
-				.then(postcssRes => {
-					fs.writeFile(path.join(themesFolder, 'FrostedGlass.theme.css'), postcssRes.css, (err) => {
-						if (err) console.error(err);
-						else console.log(`Built css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
-					})
-				})
+		dev.forEach(step => {
+			compile({
+				target: step.target,
+				output: [themesFolder, ...step.output]
+			})
 		})
-	})
+	});
